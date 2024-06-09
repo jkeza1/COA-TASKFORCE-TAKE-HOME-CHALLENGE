9a005162d5dc2ec7921e911b86154d95dd6d0b5b
Keza <144003584+jkeza1@users.noreply.github.com>
2024-06-09 14:31:57 +0200

    Update string.js

--- challenge2/string.js
+++ challenge2/string.js
@@ -1,21 +1,14 @@
 function transformString(input) {
-    // Check if the length of the string is divisible by 3
-    const isDivisibleBy3 = input.length % 3 === 0;
-    // Check if the length of the string is divisible by 5
-    const isDivisibleBy5 = input.length % 5 === 0;
+    const length = input.length;
+    const reverse = length % 3 === 0;
+    const ascii = length % 5 === 0;
 
-    // If the length is divisible by both 3 and 5, perform both operations
-    if (isDivisibleBy3 && isDivisibleBy5) {
-        // Reverse the entire string
-        input = input.split('').reverse().join('');
-        // Replace each character with its ASCII code
-        input = input.split('').map(char => char.charCodeAt(0)).join(' ');
-    } else if (isDivisibleBy3) {
-        // If divisible by 3, reverse the string
-        input = input.split('').reverse().join('');
-    } else if (isDivisibleBy5) {
-        // If divisible by 5, replace each character with its ASCII code
-        input = input.split('').map(char => char.charCodeAt(0)).join(' ');
+    if (reverse && ascii) {
+        return input.split('').reverse().map(char => char.charCodeAt(0)).join(' ');
+    } else if (reverse) {
+        return input.split('').reverse().join('');
+    } else if (ascii) {
+        return input.split('').map(char => char.charCodeAt(0)).join(' ');
     }
 
     return input;
